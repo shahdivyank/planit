11326c28ac9b1cf7afd786df13840e7b9ff184b7
shahdivyank <divyank.shah.2016@gmail.com>
2024-09-06 03:16:11 -0700

    setup process

--- src/components/landing/process/index.tsx
+++ src/components/landing/process/index.tsx
@@ -1,5 +1,26 @@
+import Arrow from "./arrow";
+import Description from "./description";
+import Discover from "./discover";
+import Go from "./go";
+import Plan from "./plan";
+import Tile from "./tile";
+
 const Process = () => {
-  return <div>Process</div>;
+  return (
+    <div>
+      <Tile />
+      <Tile />
+      <Tile />
+
+      <Arrow />
+      <Description />
+      <Arrow />
+
+      <Discover />
+      <Plan />
+      <Go />
+    </div>
+  );
 };
 
 export default Process;
